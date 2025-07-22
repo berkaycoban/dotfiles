dd94daac75df98f6fba3a9bda17e0e4052a93fee
berkaycoban <bcobann35@gmail.com>
2025-07-22 12:54:44 +0300

    refactor: simplify SSH key generation process in ssh_setup.sh

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -7,13 +7,14 @@ SSH_KEY_PATH="$HOME/.ssh/$KEY_NAME"
 echo ">> Generating new SSH key: $KEY_NAME"
 
 mkdir -p "$HOME/.ssh"
-ssh-keygen -t rsa -b 4096 -f "$SSH_KEY_PATH" -N "" -C "berkaycoban@github"
 
-# Automatically add the key to ssh-agent and keychain
-eval "$(ssh-agent -s)"
-/usr/bin/ssh-add --apple-use-keychain "$SSH_KEY_PATH"
+ssh-keygen -f $SSH_KEY_PATH
+
+ssh-add $SSH_KEY_PATH
+/usr/bin/ssh-add $SSH_KEY_PATH
 
 echo "✅ New SSH key generated at $SSH_KEY_PATH"
 cat "${SSH_KEY_PATH}.pub" | pbcopy
+
 echo "📋 Public key copied to clipboard."
 echo "👉 Paste it to GitHub: https://github.com/settings/keys"
